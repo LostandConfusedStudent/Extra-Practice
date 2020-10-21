f35cbca81c5203064f609b6b0050fa7e4c2c0d89
pausun <pfsun9999@msn.com>
2020-10-20 19:59:56 -0800

    Worked on JavaScript for page 4

---
+++ assets/js/page4.js
@@ -0,0 +1,25 @@
+// Print out a message in console
+console.log("Testing...1, 2, 3...");
+console.log("-----------------------");
+
+// Declare array
+var pets = ["dog", "cat", "fish", "bird"];
+
+// Print out array
+console.log(pets);
+console.log("-----------------------");
+
+// Let's target each pet and print them out one by one
+console.log(pets[0]);
+console.log(pets[1]);
+console.log(pets[2]);
+console.log(pets[3]);
+console.log("-----------------------");
+
+// Do it again with a for loop
+for (var i = 0; i < pets.length; i++) {
+    console.log(pets[i]);
+}
+console.log("-----------------------");
+
+// Let's do some prompts and confirms
